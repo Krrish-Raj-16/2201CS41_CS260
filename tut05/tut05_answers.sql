1c24d01ab3e8678a1a260891eac90cbec40ee7e6
Krrish Raj <113225511+Krrish-Raj-16@users.noreply.github.com>
2024-04-16 01:39:42 +0530

    Update tut05_answers.sql

--- tut05/tut05_answers.sql
+++ tut05/tut05_answers.sql
@@ -2,6 +2,9 @@
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
 
+create database data3;
+use data3;
+
 CREATE TABLE departments (
     department_id INT PRIMARY KEY,
     department_name VARCHAR(255),
